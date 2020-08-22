914fb09590286e4885ece60df92e71a892c87781
EzHawkeye <66911686+EzHawkeye@users.noreply.github.com>
2020-08-22 20:17:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -69,8 +69,7 @@ bot.on("message", async message => {
 
     if (message.channel.id === SUGGESTION_CHANNEL) {
         let embed = new Discord.RichEmbed()
-        .setAuthor(message.member.nickname ? message.member.nickname : message.author.tag, message.author.avatarURL)
-        .setThumbnail(message.author.displayAvatarURL())
+        .setAuthor(message.member.nickname ? message.member.nickname : message.author.tag, message.author.displayAvatarURL())
         .setColor(1146986)
         .setTitle('Community Suggestions')
         .setDescription(message.content)
